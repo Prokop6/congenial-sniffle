9b1ad538471bd7690bdbf5f6d5a0fe50f9f8c9cb
Marcin Prokop <mr.prokop.6+dev@gmail.com>
2022-07-18 22:12:53 +0200

    start of test suite

--- test/board_methods.ts
+++ test/board_methods.ts
@@ -10,4 +10,46 @@ describe(
     ({cell, counter, expected}) => expect(isTrue(cell, counter)).toBe(expected)
     )
   )
-)
+);
+
+/*
+.each(
+  [
+   , [
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0]
+    ], [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 1]
+    ]
+  ]
+)(
+*/
+  
+
+describe('test cell value recognition in situ',
+  () => (
+    test.todo.each([
+      { x: 0, y: 0, expected: true },
+      { x: 1, y: 0, expected: true },
+      { x: 2, y: 0, expected: true },
+      { x: 0, y: 1, expected: true },
+      { x: 1, y: 1, expected: true },
+      { x: 2, y: 1, expected: true },
+      { x: 0, y: 2, expected: true },
+      { x: 1, y: 2, expected: true },
+      { x: 2, y: 2, expected: true }
+    ])('fdsf', () => {
+          let cellArray = [
+          [1, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]
+        ]; (
+        { x, y, expected }
+        ) => ( expect(isCellAlive(cellArray, x, y).toBe(expected)) )
+      }
+    )
+  )
+);
